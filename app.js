89f9e15522b97effcb04745e9bb5b005b0254a63
TimofeiShpak <tima.shpak.v@gmail.com>
2021-09-13 15:38:33 +0400

    change

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const express = require("express");
-const app = express();
-const MongoClient = require("mongodb").MongoClient;
-const objectId = require("mongodb").ObjectId;
-app.use(express.static(__dirname + "/public"));
-// import the routes file
-const routes = require("./routes/routes")
+// const express = require("express");
+// const app = express();
+// app.use(express.static(__dirname + "/public"));
+// // import the routes file
+// const routes = require("./routes/routes")
 
-// body parser configuration
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// // body parser configuration
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// register the routes 
-app.use('/', routes);
+// // register the routes 
+// app.use('/', routes);
 
-module.exports = app;
+// module.exports = app;
--- index.js
+++ index.js
@@ -1,47 +1,123 @@
-const http = require('http');
-const app = require('./app');
+// const http = require('http');
+// const app = require('./app');
 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
+// const normalizePort = val => {
+//   const port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+//   if (isNaN(port)) {
+//     return val;
+//   }
+//   if (port >= 0) {
+//     return port;
+//   }
+//   return false;
+// };
+// const port = normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
 
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+// const errorHandler = error => {
+//   if (error.syscall !== 'listen') {
+//     throw error;
+//   }
+//   const address = server.address();
+//   const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+//   switch (error.code) {
+//     case 'EACCES':
+//       console.error(bind + ' requires elevated privileges.');
+//       process.exit(1);
+//       break;
+//     case 'EADDRINUSE':
+//       console.error(bind + ' is already in use.');
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// };
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+// server.on('error', errorHandler);
+// server.on('listening', () => {
+//   const address = server.address();
+//   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+//   console.log('Listening on ' + bind);
+// });
+
+// server.listen(port);
+
+
+const mongoose = require("mongoose");
+const express = require("express");
+const Schema = mongoose.Schema;
+const app = express();
+const jsonParser = express.json();
+ 
+const userScheme = new Schema({name: String, age: Number}, {versionKey: false});
+const User = mongoose.model("User", userScheme);
+ 
+app.use(express.static(__dirname + "/public"));
+ 
+mongoose.connect("mongodb+srv://admin:Hora1234@cluster0.ouwqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, function(err){
+    if(err) return console.log(err);
+    app.listen(3000, function(){
+        console.log("Сервер ожидает подключения...");
+    });
 });
-
-server.listen(port);
+  
+app.get("/api/users", function(req, res){
+        
+    User.find({}, function(err, users){
+ 
+        if(err) return console.log(err);
+        res.send(users)
+    });
+});
+ 
+app.get("/api/users/:id", function(req, res){
+         
+    const id = req.params.id;
+    User.findOne({_id: id}, function(err, user){
+          
+        if(err) return console.log(err);
+        res.send(user);
+    });
+});
+    
+app.post("/api/users", jsonParser, function (req, res) {
+        
+    if(!req.body) return res.sendStatus(400);
+        
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const user = new User({name: userName, age: userAge});
+        
+    user.save(function(err){
+        if(err) return console.log(err);
+        res.send(user);
+    });
+});
+     
+app.delete("/api/users/:id", function(req, res){
+         
+    const id = req.params.id;
+    User.findByIdAndDelete(id, function(err, user){
+                
+        if(err) return console.log(err);
+        res.send(user);
+    });
+});
+    
+app.put("/api/users", jsonParser, function(req, res){
+         
+    if(!req.body) return res.sendStatus(400);
+    const id = req.body.id;
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const newUser = {age: userAge, name: userName};
+     
+    User.findOneAndUpdate({_id: id}, newUser, {new: true}, function(err, user){
+        if(err) return console.log(err); 
+        res.send(user);
+    });
+});
