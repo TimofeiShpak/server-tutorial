2fd24cd8e0a21208a77b5a381e03eed76874a320
TimofeiShpak <tima.shpak.v@gmail.com>
2021-09-13 15:59:38 +0400

    change

--- index.js
+++ index.js
@@ -46,77 +46,171 @@
 
 // server.listen(port);
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+// const express = require("express");
+// const Schema = mongoose.Schema;
+// const app = express();
+// const jsonParser = express.json();
+ 
+// const userScheme = new Schema({name: String, age: Number}, {versionKey: false});
+// const User = mongoose.model("User", userScheme);
+ 
+// app.use(express.static(__dirname + "/public"));
+ 
+// mongoose.connect("mongodb+srv://admin:Hora1234@cluster0.ouwqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true }, function(err){
+//     if(err) return console.log(err);
+//     app.listen(3000, function(){
+//         console.log("Сервер ожидает подключения...");
+//     });
+// });
+  
+// app.get("/api/users", function(req, res){
+        
+//     User.find({}, function(err, users){
+ 
+//         if(err) return console.log(err);
+//         res.send(users)
+//     });
+// });
+ 
+// app.get("/api/users/:id", function(req, res){
+         
+//     const id = req.params.id;
+//     User.findOne({_id: id}, function(err, user){
+          
+//         if(err) return console.log(err);
+//         res.send(user);
+//     });
+// });
+    
+// app.post("/api/users", jsonParser, function (req, res) {
+        
+//     if(!req.body) return res.sendStatus(400);
+        
+//     const userName = req.body.name;
+//     const userAge = req.body.age;
+//     const user = new User({name: userName, age: userAge});
+        
+//     user.save(function(err){
+//         if(err) return console.log(err);
+//         res.send(user);
+//     });
+// });
+     
+// app.delete("/api/users/:id", function(req, res){
+         
+//     const id = req.params.id;
+//     User.findByIdAndDelete(id, function(err, user){
+                
+//         if(err) return console.log(err);
+//         res.send(user);
+//     });
+// });
+    
+// app.put("/api/users", jsonParser, function(req, res){
+         
+//     if(!req.body) return res.sendStatus(400);
+//     const id = req.body.id;
+//     const userName = req.body.name;
+//     const userAge = req.body.age;
+//     const newUser = {age: userAge, name: userName};
+     
+//     User.findOneAndUpdate({_id: id}, newUser, {new: true}, function(err, user){
+//         if(err) return console.log(err); 
+//         res.send(user);
+//     });
+// });
+
 const express = require("express");
-const Schema = mongoose.Schema;
+const MongoClient = require("mongodb").MongoClient;
+const objectId = require("mongodb").ObjectId;
+   
 const app = express();
 const jsonParser = express.json();
- 
-const userScheme = new Schema({name: String, age: Number}, {versionKey: false});
-const User = mongoose.model("User", userScheme);
- 
+ 
+const mongoClient = new MongoClient("mongodb://localhost:27017/");
+ 
+let dbClient;
+ 
 app.use(express.static(__dirname + "/public"));
- 
-mongoose.connect("mongodb+srv://admin:Hora1234@cluster0.ouwqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useUnifiedTopology: true, useNewUrlParser: true }, function(err){
-    if(err) return console.log(err);
-    app.listen(3000, function(){
-        console.log("Сервер ожидает подключения...");
-    });
+ 
+mongoClient.connect(function(err, client){
+    if(err) return console.log(err);
+    dbClient = client;
+    app.locals.collection = client.db("usersdb").collection("users");
+    app.listen(3000, function(){
+        console.log("Сервер ожидает подключения...");
+    });
 });
-  
+ 
 app.get("/api/users", function(req, res){
-        
-    User.find({}, function(err, users){
- 
-        if(err) return console.log(err);
-        res.send(users)
-    });
+        
+    const collection = req.app.locals.collection;
+    collection.find({}).toArray(function(err, users){
+         
+        if(err) return console.log(err);
+        res.send(users)
+    });
+     
 });
- 
 app.get("/api/users/:id", function(req, res){
-         
-    const id = req.params.id;
-    User.findOne({_id: id}, function(err, user){
-          
-        if(err) return console.log(err);
-        res.send(user);
-    });
+        
+    const id = new objectId(req.params.id);
+    const collection = req.app.locals.collection;
+    collection.findOne({_id: id}, function(err, user){
+               
+        if(err) return console.log(err);
+        res.send(user);
+    });
 });
-    
+   
 app.post("/api/users", jsonParser, function (req, res) {
-        
-    if(!req.body) return res.sendStatus(400);
-        
-    const userName = req.body.name;
-    const userAge = req.body.age;
-    const user = new User({name: userName, age: userAge});
-        
-    user.save(function(err){
-        if(err) return console.log(err);
-        res.send(user);
-    });
+       
+    if(!req.body) return res.sendStatus(400);
+       
+    const userName = req.body.name;
+    const userAge = req.body.age;
+    const user = {name: userName, age: userAge};
+       
+    const collection = req.app.locals.collection;
+    collection.insertOne(user, function(err, result){
+               
+        if(err) return console.log(err);
+        res.send(user);
+    });
 });
-     
+    
 app.delete("/api/users/:id", function(req, res){
-         
-    const id = req.params.id;
-    User.findByIdAndDelete(id, function(err, user){
-                
-        if(err) return console.log(err);
-        res.send(user);
-    });
+        
+    const id = new objectId(req.params.id);
+    const collection = req.app.locals.collection;
+    collection.findOneAndDelete({_id: id}, function(err, result){
+               
+        if(err) return console.log(err);    
+        let user = result.value;
+        res.send(user);
+    });
 });
-    
+   
 app.put("/api/users", jsonParser, function(req, res){
-         
-    if(!req.body) return res.sendStatus(400);
-    const id = req.body.id;
-    const userName = req.body.name;
-    const userAge = req.body.age;
-    const newUser = {age: userAge, name: userName};
-     
-    User.findOneAndUpdate({_id: id}, newUser, {new: true}, function(err, user){
-        if(err) return console.log(err); 
-        res.send(user);
-    });
+        
+    if(!req.body) return res.sendStatus(400);
+    const id = new objectId(req.body.id);
+    const userName = req.body.name;
+    const userAge = req.body.age;
+       
+    const collection = req.app.locals.collection;
+    collection.findOneAndUpdate({_id: id}, { $set: {age: userAge, name: userName}},
+         {returnDocument: "after" },function(err, result){
+               
+        if(err) return console.log(err);     
+        const user = result.value;
+        res.send(user);
+    });
+});
+ 
+// прослушиваем прерывание работы программы (ctrl-c)
+process.on("SIGINT", () => {
+    dbClient.close();
+    process.exit();
 });
